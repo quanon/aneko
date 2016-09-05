8c2ca1f0c46f6c6dbfec7f6bee866b99fd9ba869
noda <noda@haw.co.jp>
2016-09-05 20:49:13 +0900

    Install Semantic UI

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,4 +1,5 @@
 var path = require('path')
+var webpack = require('webpack')
 var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
@@ -81,5 +82,12 @@ module.exports = {
   },
   vue: {
     loaders: utils.cssLoaders()
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    })
+  ],
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import App from './App';
+import '../node_modules/semantic-ui-css/semantic.css';
+import '../node_modules/semantic-ui-css/semantic.js';
 
 /* eslint-disable no-new */
 new Vue({
